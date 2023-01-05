1d268ce8d221d7854c69285fec9015aada5f9e40
cmwakichi <mmwambela@gmail.com>
2023-01-05 16:58:37 +0300

    added a string limit for the notes text

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -21,7 +21,7 @@
                 <div class="w-full mt-3 bg-gray-100 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
                     <h2>{{ $note->title }}</h2>
                     <p class="text-gray-700 text-sm mt-2">
-                        {{ $note->text }}
+                        {{ Str::limit($note->text, 100, '...') }}
                     </p>
                     <span class="text-sm opacity-70 mt-4 block">{{ date('jS M Y', strtotime($note->updated_at)) }}</span>
                 </div>

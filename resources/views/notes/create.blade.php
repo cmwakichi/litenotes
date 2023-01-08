0552a903ccb14548092bd0723a529da30796f7b9
cmwakichi <mmwambela@gmail.com>
2023-01-08 22:21:38 +0300

    added code to save the old input details

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -6,14 +6,14 @@
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                 <form action="/notes" method="POST">
                     @csrf
-                    <input type="text" name="title" placeholder="Title..." autocomplete="off"
+                    <input type="text" value="{{ @old('title') }}" name="title" placeholder="Title..." autocomplete="off"
                         class="w-full mb-3 outline-none" required>
                     @error('title')
                         <div class="text-red-300 text-sm">{{ $message }}
                         </div>
                     @enderror
                     <textarea name="text" rows="10" placeholder="Text..." class="mb-3 w-full outline-none md:tracking-wide"
-                        required></textarea>
+                        required>{{ @old('text') }}</textarea>
                     @error('text')
                         <div class="text-red-300 text-sm">{{ $message }}
                         </div>

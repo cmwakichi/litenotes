a05cf85aa8fe98890871168644d84af46cb8b858
cmwakichi <mmwambela@gmail.com>
2023-01-08 22:04:38 +0300

    replaced error with the error message

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -9,13 +9,13 @@
                     <input type="text" name="title" placeholder="Title..." autocomplete="off"
                         class="w-full mb-3 outline-none" required>
                     @error('title')
-                        <div class="text-red-300 text-sm">{{ $error }}
+                        <div class="text-red-300 text-sm">{{ $message }}
                         </div>
                     @enderror
                     <textarea name="text" rows="10" placeholder="Text..." class="mb-3 w-full outline-none md:tracking-wide"
                         required></textarea>
                     @error('text')
-                        <div class="text-red-300 text-sm">{{ $error }}
+                        <div class="text-red-300 text-sm">{{ $message }}
                         </div>
                     @enderror
                     <button type="submit" class="mt-6 rounded-md bg-blue-900 p-2">Save note</button>

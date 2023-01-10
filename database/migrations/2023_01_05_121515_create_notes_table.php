1b196872d746200dcd15a4d3548eeb029761309d
cmwakichi <mmwambela@gmail.com>
2023-01-10 11:58:25 +0300

    added a new uuid column to the notes migration

--- database/migrations/2023_01_05_121515_create_notes_table.php
+++ database/migrations/2023_01_05_121515_create_notes_table.php
@@ -16,6 +16,7 @@ class CreateNotesTable extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
+            $table->uuid('uuid');
             $table->string('title');
             $table->longText('text');
             $table->timestamps();

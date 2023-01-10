f387a0ce2d1ba6868f446ac7e176a39f0d34bd52
cmwakichi <mmwambela@gmail.com>
2023-01-10 12:48:52 +0300

    changed notes routekeyname to uuid

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -10,4 +10,9 @@ class Note extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'title', 'text'];
+
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 }

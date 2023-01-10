126e7747b25711dc0f5a739641345db96688c0b9
cmwakichi <mmwambela@gmail.com>
2023-01-10 12:45:30 +0300

    passed the model note to the note controller show method

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -62,9 +62,9 @@ class NoteController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Note $note)
     {
-        $note = Note::where('id', '=', $id)->where('user_id', '=', Auth::id())->findOrFail($id);
+        //$note = Note::where('id', '=', $id)->where('user_id', '=', Auth::id())->findOrFail($id);
 
         return view('notes.show')->with('note', $note);
     }

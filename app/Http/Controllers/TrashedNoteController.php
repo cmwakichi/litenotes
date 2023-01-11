03ad6d6ef364affbde5697cf6e6b2d303d213bf8
cmwakichi <mmwambela@gmail.com>
2023-01-11 18:59:49 +0300

    added a index method in the trashed controller

--- app/Http/Controllers/TrashedNoteController.php
+++ app/Http/Controllers/TrashedNoteController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TrashedNoteController extends Controller
 {
-    //
+    public function index()
+    {
+        $trashednotes = Note::where('user_id', '=', Auth::id())->onlyTrashed()->latest('deteted_at')->paginate(10)->get();
+
+        return view('trash.index')->with('trashednotes', $trashednotes);
+    }
 }

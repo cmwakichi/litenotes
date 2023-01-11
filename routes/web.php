0a045e6adf36d24b23c59c95e0b5dcee52464480
cmwakichi <mmwambela@gmail.com>
2023-01-11 18:14:51 +0300

    added route to trashed notes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Auth::routes();
 //Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('notes', 'NoteController')->middleware(['auth']);
+
+Route::get('/trashed', 'TrashedNotesController@index')->middleware('auth');
